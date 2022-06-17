08806776cde09a8db06d47ec7f62c4b185ca84a9
jameswigg04@gmail.com <jameswigg04@gmail.com>
2022-06-17 18:02:36 +0000

    Encoded most codes and made an untested transmit function

--- UBMP420-Intro-5-Analog-Input.X/Intro-5-Analog-Input.c
+++ UBMP420-Intro-5-Analog-Input.X/Intro-5-Analog-Input.c
@@ -20,8 +20,21 @@
 #define	SONY_VOLDN  0x13        // Volume down button command code
 #define SONY_POWER  0x15        // Power button command code
 
+#define SHARP_POWER     0b01101000
+#define SHARP_ENTER     //TODO: Get the code
+#define SHARP_UP        0b11101010
+#define SHARP_RIGHT     0b01101111
+#define SHARP_DOWN      0b00011010
+#define SHARP_LEFT      0b10101111
+#define SHARP_VOLUP     0b00101000
+#define SHARP_VOLDOWN   0b10101000
+#define SHARP_CHUP      //TODO: Get the code
+#define SHARP_CHDOWN    //TODO: Get the code
+#define SHARP_MENU      0b00000100
+
 // Sony IR device/address codes
-#define SONY_TV     0x01        // TV device address code
+#define SONY_TV     0x01        // Sony TV device address code
+#define SHARP_TV    0b10000     // Sharp TV address code
 
 // Sony IR protocol timing and definitions
 #define SONY_START_PULSES   96  // Number of 40kHz IR pulses to make Start bit
@@ -30,6 +43,13 @@
 #define SONY_BIT_DELAY  600     // Delay between bits in microseconds
 #define SONY_DATABITS   7       // Number of data/command bits to send
 #define SONY_ADDRBITS   5       // Number of address/device bits to send
+
+
+#define SHARP_PULSES 12 //Number of IR pulses for either bit. Technically 12.16 pulses to make 320us
+#define SHARP_ZERO_BIT_DELAY 20 //Delay between bits for a zero: 680us
+#define SHARP_ONE_BIT_DELAY 1680 //Delay between bits for a one: 1680us
+#define SHARP_DATABITS   8       // Number of data/command bits to send
+#define SHARP_ADDRBITS   5       // Number of address/device bits to send
 
 
 // Program variable definitions
@@ -48,25 +68,104 @@ void pulse_40kHz(unsigned int pulses)
 
 void pulse_38kHz(unsigned int pulses)
 {
-
+    for(pulses; pulses != 0; pulses--)
+    {
+        IRLED = 1;
+        _delay(156);
+        IRLED = 0;
+        _delay(143);
+    }
 }
 
-void transmit_NEC(unsigned char address, unsigned char command)
+//EXP is always 1, CHK is always 0
+void transmit_SHARP(unsigned char address, unsigned char command)
 {
     // Save !(address), !(command)
     
-    // Send start pulse
-    
-    // Start pulse delay
-    
     // Transmit address
-   
+    for(bits = SHARP_ADDRBITS; bits != 0; bits--)
+    {
+        pulse_38kHz(SHARP_PULSES);
+        if((address & 0b00000001) == 0)
+        {
+            __delay_us(SHARP_ZERO_BIT_DELAY);
+        }
+        else
+        {
+            __delay_us(SHARP_ONE_BIT_DELAY);
+        }
+        address = address >> 1;
+    }
     // Transmit !(address)
     
     // Transmit command
-    
+    for(bits = SHARP_DATABITS; bits != 0; bits--)
+    {
+        pulse_38kHz(SHARP_PULSES);
+        if((command & 0b00000001) == 0)
+        {
+            __delay_us(SHARP_ZERO_BIT_DELAY);
+        }
+        else
+        {
+            __delay_us(SHARP_ONE_BIT_DELAY);
+        }
+        command = command >> 1;
+    }
+
     // Transmit !(command)
+
+    //Transmit EXP (Always 1 uninverted)
+    pulse_38kHz(SHARP_PULSES);
+    __delay_us(SHARP_ONE_BIT_DELAY);
+
+    //Transmit CHK (Always 0 uninverted)
+    pulse_38kHz(SHARP_PULSES);
+    __delay_us(SHARP_ZERO_BIT_DELAY);
+
+    //Delay between uninverted and inverted signal
+    __delay_us(40);
+
+    //Now do it with the command, EXP, and CHK inverted
     
+    // Transmit address
+    for(bits = SHARP_ADDRBITS; bits != 0; bits--)
+    {
+        pulse_38kHz(SHARP_PULSES);
+        if((address & 0b00000001) == 0)
+        {
+            __delay_us(SHARP_ZERO_BIT_DELAY);
+        }
+        else
+        {
+            __delay_us(SHARP_ONE_BIT_DELAY);
+        }
+        address = address >> 1;
+    }
+
+    // Transmit command inverted
+    for(bits = SHARP_DATABITS; bits != 0; bits--)
+    {
+        pulse_38kHz(SHARP_PULSES);
+        if((command & 0b10000000) == 0)
+        {
+            __delay_us(SHARP_ZERO_BIT_DELAY);
+        }
+        else
+        {
+            __delay_us(SHARP_ONE_BIT_DELAY);
+        }
+        command = command << 1;
+    }
+
+    //Transmit EXP inverted (Always 0 inverted)
+    pulse_38kHz(SHARP_PULSES);
+    __delay_us(SHARP_ZERO_BIT_DELAY);
+
+    //Transmit CHK (Always 1 inverted)
+    pulse_38kHz(SHARP_PULSES);
+    __delay_us(SHARP_ONE_BIT_DELAY);
+
 }
 
 void transmit_Sony(unsigned char address, unsigned char command)
@@ -104,7 +203,7 @@ int main(void)
     while(1)
 	{
         // Pulse timing test code
-        pulse_40kHz(100);
+        pulse_38kHz(100);
         
         // Read pushbuttons
         
